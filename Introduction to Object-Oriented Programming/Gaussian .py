e4b25b2c8a33e22ecd9208729ad1279e2c8629c5
unknown <kabuur7@gmail.com>
2024-01-10 10:23:51 -0800

    Python for Ai programming

---
+++ Introduction to Object-Oriented Programming/Gaussian .py
@@ -0,0 +1,35 @@
+"""This lesson is going to introduce the idea of packages through a Gaussian Distribution example.
+
+
+Include in this Python package will be :
+
+Read in dataset
+Calculate mean
+Calculate standard deviation
+Plot histogram
+Plot probability density function
+add two gaussian distributions"""
+
+
+
+
+"""Here are a few formulas that might be helpful:
+
+Gaussian Distribution Formulas
+probability density function"""
+
+"""Binomial Distribution Formulas
+mean"""
+
+"""In other words, a fair coin has a probability of a positive outcome (heads) p = 0.5. If you flip a coin 20 times, the mean would be 20 * 0.5 = 10; you'd expect to get 10 heads.
+
+variance"""
+
+"""Continuing with the coin example, n would be the number of coin tosses and p would be the probability of getting heads.
+
+standard deviation"""
+
+"""or in other words, the standard deviation is the square root of the variance.
+
+probability density function
+"""
